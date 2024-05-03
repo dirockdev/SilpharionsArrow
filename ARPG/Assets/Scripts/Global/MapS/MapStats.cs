450d7553b675434e36396722d47a09814829b349
dirockdev <droca.22.d@gmail.com>
2024-05-03 20:53:34 +0200

    New torch and portals
    Portal script and prefabs

---
+++ ARPG/Assets/Scripts/Global/MapS/MapStats.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="MapStats")]
+public class MapStats:ScriptableObject
+{
+    public float intensityLight;
+    public Color colorLight;
+    public int kelvin;
+
+}
